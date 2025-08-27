1fad63d6635fe9fad6f0d7d0a3e2b2d916334aef
hhjjfly-alt <hhjjfly@gmail.com>
2025-08-27 11:17:39 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -15,7 +15,7 @@ echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-inf
 
 # passwall
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 
 # iStore
 echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
